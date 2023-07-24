e8c5d7bcb06c0e9e9e49816fc929054ac78c9799
Sheerin Sultana <48320832+SheerinSultana@users.noreply.github.com>
2023-07-24 16:46:45 +0400

    Create Arduino - Automatic Street Light.cpp

---
+++ Arduino - Automatic Street Light.cpp
@@ -0,0 +1,14 @@
+LDR -  #10
+LED -  #3
+
+int LDRPin = 10;
+int led = 3;
+void setup() { 
+pinMode(led, OUTPUT);
+pinMode(LDRPin,INPUT); }
+void loop()
+{
+if (digitalRead(LDRPin)==HIGH)
+{digitalWrite(led,HIGH);}
+else{digitalWrite(led,LOW);}
+}
